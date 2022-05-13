75dc5d16e184574dde2fa817ecfd029004395ca5
Abiscula <victor.augusto@bcrservicos.com.br>
2022-05-13 14:24:23 -0300

    Criado rota de post para subir arquivo xlsx para o backend
    Adicionado função para salvar arquivo temporariamente no computador.
    Adicionado função para converter xlsx em json.

---
+++ src/routes.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import formidable from 'formidable';
+import excelToJson from 'convert-excel-to-json';
+import path from 'path';
+
+export const routes = express.Router()
+
+routes.post('/table', (req, res) => {
+    const form = formidable({ multiples: true })
+
+    form.parse(req, (err, fields, files: any) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        const filePath = files.table.filepath
+        const resolvedPath = filePath.split(path.sep).join(path.posix.sep);
+
+        const result = excelToJson({
+            sourceFile: resolvedPath
+        })
+
+        console.log(result)
+    })
+})
